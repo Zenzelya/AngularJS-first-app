89e99428e9ca25c258a141e82ada26430c2e4c1d
Andrei Kelhankin <avkelhankin@gmail.com>
2018-12-25 20:31:08 +0200

    Clean code

--- js/app.show.cvs/app.showcvs.ctrl.js
+++ js/app.show.cvs/app.showcvs.ctrl.js
@@ -1,42 +1,40 @@
 angular
-    .module('app.show.csv', [])
-    .controller('ShowCsvCtrl', ShowCsvCtrl);
+  .module('app.show.csv', [])
+  .controller('ShowCsvCtrl', ShowCsvCtrl);
 
- ShowCsvCtrl.$inject = ['parseToSentences'];
+ShowCsvCtrl.$inject = ['parseToSentences'];
 
 function ShowCsvCtrl(parseToSentences) {
-		var vm = this;
-		vm.text = parseToSentences.getInputText();
-		vm.max_length = 0; 
-		vm.sentences;
- 
+  var vm = this;
+  vm.text = parseToSentences.getInputText();
+  vm.max_length = 0;
+  vm.sentences;
 
-		vm.getMaxSentenceSize = getMaxSentenceSize ;
-		vm.showCVS = showCVS;
 
-		
+  vm.getMaxSentenceSize = getMaxSentenceSize;
+  vm.showCVS = showCVS;
 
-		function showCVS () {
-			vm.text = parseToSentences.getInputText();
-			if (vm.text) {
-			vm.sentences = parseToSentences.toWords(parseToSentences.toSentences(vm.text));
-			vm.max_length = vm.getMaxSentenceSize();
-			
-			}
-			else {
-				alert('Please enter text');
-			}
-		}
 
-		function getMaxSentenceSize(){
-			var max = 0;
+  function showCVS() {
+    vm.text = parseToSentences.getInputText();
+    if (vm.text) {
+      vm.sentences = parseToSentences.toWords(parseToSentences.toSentences(vm.text));
+      vm.max_length = vm.getMaxSentenceSize();
 
-			for (let i = 0; i < vm.sentences.length; i++) {
-				if (max < vm.sentences[i].length){
-					max = vm.sentences[i].length;
-				}
-			}
-			return max;
-		}
-		
- }
+    } else {
+      alert('Please enter text');
+    }
+  }
+
+  function getMaxSentenceSize() {
+    var max = 0;
+
+    for (let i = 0; i < vm.sentences.length; i++) {
+      if (max < vm.sentences[i].length) {
+        max = vm.sentences[i].length;
+      }
+    }
+    return max;
+  }
+
+}
--- js/toxml.js
+++
@@ -1,4 +0,0 @@
-angular.module('toXml', ['parseToSentences'])
-				.controller('toXmlCtrl', [ 'parseToSentences', '$scope', function ($scope, parseToSentences){
-					$scope.text = '1';
-				}]);
