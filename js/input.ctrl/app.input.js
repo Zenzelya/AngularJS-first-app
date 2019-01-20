6c95f03d79c7d633346e034bfcbb9667d9749614
Andrei Kelhankin <avkelhankin@gmail.com>
2019-01-20 17:09:07 +0200

    new interface

--- js/input.ctrl/app.input.js
+++ js/input.ctrl/app.input.js
@@ -1,12 +1,11 @@
 (function () {
 'use strict';
-angular
-    .module('app.input', [])
-    .controller('InputCtrl', InputCtrl);
+angular.module('app.input', [])
+			 .controller('InputCtrl', InputCtrl);
 
  InputCtrl.$inject = ['parseToSentences'];
 
-function InputCtrl(parseToSentences) {
+	function InputCtrl(parseToSentences) {
 		let vm = this;
 		vm.text = 'Mary had a little lamb. Peter called for the wolf, and Aesop came. Cinderella likes shoes';
 		vm.onChange = onChange;
@@ -16,5 +15,5 @@ function InputCtrl(parseToSentences) {
 		function onChange() {
 			parseToSentences.setInputText(vm.text);
 		}
- }
+ 	}
  })();
